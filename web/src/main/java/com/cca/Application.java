188b4f04994490f2585e95571c950411f407731a
chengchangan <cheng.changan@eascs.com>
2021-02-23 14:59:04 +0800

    注释测试启动类

--- web/src/main/java/com/cca/Application.java
+++ web/src/main/java/com/cca/Application.java
@@ -11,9 +11,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * @version 1.0
  * @date 2021/1/23 14:33
  */
-@EnableWebMvc //todo 测试专用
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
-        JpaRepositoriesAutoConfiguration.class}, scanBasePackages = "com.cca")
+//@EnableWebMvc //todo 测试专用
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+//        JpaRepositoriesAutoConfiguration.class}, scanBasePackages = "com.cca")
 public class Application {
 
     public static void main(String[] args) {
