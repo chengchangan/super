f36774a137e555e8419d3314d786e1820b54ade2
chengchangan <cheng.changan@eascs.com>
2021-07-14 17:27:27 +0800

    流程处理增加配置的方式

--- flow/src/main/java/io/boncray/flow/process/ProcessLoader.java
+++ flow/src/main/java/io/boncray/flow/process/ProcessLoader.java
@@ -1,12 +1,24 @@
 package io.boncray.flow.process;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.io.CharStreams;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author cca
@@ -17,24 +29,68 @@ import java.util.List;
 @Component
 public class ProcessLoader {
 
-    @Autowired
-    private ProcessRegistry processRegistry;
+    /**
+     * default node paths
+     */
+    private static final String DEFAULT_PATHS = "PROCESS-FLOW/process_flow_*.json";
 
+    /**
+     * <p>
+     * custom node paths
+     */
+    @Value("${process.flow.custom.location:PROCESS-FLOW/custom_process_flow_*.json}")
+    private String customPath;
+    @Value("${process.flow.overrideDefaultNodes:true}")
+    private Boolean overrideDefaultNodes;
+
+    private final ApplicationContext applicationContext;
+    private final ProcessRegistry processRegistry;
+
+    public ProcessLoader(ApplicationContext applicationContext, ProcessRegistry processRegistry) {
+        this.applicationContext = applicationContext;
+        this.processRegistry = processRegistry;
+    }
 
     @PostConstruct
-    public void init() {
-        // todo
-        Process process = new Process();
-        process.setBizCode("translate");
-        process.setOperation("create");
-        List<String> list = new ArrayList<>();
-        list.add("initTranslate");
-        list.add("productTranslate");
-        list.add("addressTranslate");
-        list.add("memberTranslate");
-        list.add("finishedTranslate");
-        process.setNodeList(list);
-        processRegistry.registry(process);
+    public void init() throws IOException {
+        try {
+            log.info("[START_LOAD_PROCESS_FLOWS] defaultPath: {}, customPath: {}, overrideDefaultNodes: {}",
+                    DEFAULT_PATHS, customPath, overrideDefaultNodes);
+
+            List<Process> defaultProcess = loadProcessConfig(DEFAULT_PATHS);
+            List<Process> customProcess = loadProcessConfig(customPath);
+
+            // 校验自定义的与默认的是否冲突
+            if (overrideDefaultNodes) {
+                if (!Collections.disjoint(defaultProcess, customProcess)) {
+                    throw new RuntimeException("trade.duplicate.process.config");
+                }
+            }
+            // default merge custom put all to registry
+            Set<Process> processSet = Sets.newHashSet(customProcess);
+            processSet.addAll(defaultProcess);
+            processRegistry.registryAll(processSet);
+        } catch (Throwable e) {
+            log.error("load process flow error: {}", Throwables.getStackTraceAsString(e));
+            throw e;
+        }
+    }
+
+    /**
+     * load process
+     *
+     * @param resourcePath resource path
+     * @return list of process
+     * @throws IOException
+     */
+    protected List<Process> loadProcessConfig(String resourcePath) throws IOException {
+        List<Process> processes = Lists.newArrayList();
+        Resource[] resources = applicationContext.getResources(ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + resourcePath);
+        for (Resource resource : resources) {
+            String json = CharStreams.toString(new InputStreamReader(resource.getInputStream(), Charsets.UTF_8));
+            processes.add(JSON.parseObject(json, Process.class));
+        }
+        return processes;
     }
 
 }
