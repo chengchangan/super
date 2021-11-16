6d0de1183f4413ece1b3c2cd9166fd98228a9a26
changan <changan.cheng@hairobitics.com>
2021-11-16 19:33:32 +0800

    增加集合处理工具方法

--- common/src/main/java/io/boncray/common/utils/CollectionsUtil.java
+++ common/src/main/java/io/boncray/common/utils/CollectionsUtil.java
@@ -2,9 +2,7 @@ package io.boncray.common.utils;
 
 import org.springframework.util.CollectionUtils;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -24,7 +22,7 @@ public class CollectionsUtil {
      */
     public static <T, K> Map<K, List<T>> listToMapList(List<T> list, Function<? super T, ? extends K> classifier) {
         if (CollectionUtils.isEmpty(list)) {
-            return Collections.emptyMap();
+            return new HashMap<>();
         }
         return list.stream().collect(Collectors.groupingBy(classifier));
     }
@@ -38,8 +36,42 @@ public class CollectionsUtil {
      */
     public static <T, K> Map<K, T> listToMap(List<T> list, Function<? super T, ? extends K> classifier) {
         if (CollectionUtils.isEmpty(list)) {
-            return Collections.emptyMap();
+            return new HashMap<>();
         }
         return list.stream().collect(Collectors.toMap(classifier, x -> x));
     }
+
+    /**
+     * 集合数据提取
+     *
+     * @param list       源数据
+     * @param classifier 提取数据的function
+     * @param <T>        元数据对象类型
+     * @param <K>        提取数据类型
+     * @return 返回提取后的数据列表
+     */
+    public static <T, K> List<K> fetchToList(List<T> list, Function<? super T, ? extends K> classifier) {
+        if (CollectionUtils.isEmpty(list)) {
+            return new ArrayList<>();
+        }
+        return list.stream().map(classifier).collect(Collectors.toList());
+    }
+
+    /**
+     * 集合数据提取
+     *
+     * @param list       源数据
+     * @param classifier 提取数据的function
+     * @param <T>        元数据对象类型
+     * @param <K>        提取数据类型
+     * @return 返回提取后的数据列表
+     */
+    public static <T, K> Set<K> fetchToSet(List<T> list, Function<? super T, ? extends K> classifier) {
+        if (CollectionUtils.isEmpty(list)) {
+            return new HashSet<>();
+        }
+        return list.stream().map(classifier).collect(Collectors.toSet());
+    }
+
+
 }
